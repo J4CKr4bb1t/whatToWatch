a396bf9bdeff86740a8a91d74d057c97eaaf7c64
jubck <julialbock@gmail.com>
2024-04-16 14:30:21 -0400

    navigation for streaming fragment

--- app/src/main/java/com/hfad/whattowatch/StreamingFragment.kt
+++ app/src/main/java/com/hfad/whattowatch/StreamingFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.findNavController
 import com.hfad.whattowatch.databinding.FragmentStreamingBinding
 
 class StreamingFragment : Fragment() {
@@ -18,7 +19,13 @@ class StreamingFragment : Fragment() {
         val view = binding.root
         return view
     }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.returnToSearchFromStreaming.setOnClickListener {
+            it.findNavController().navigate(R.id.action_streamingFragment_to_searchFragment)
+        }
 
+    }
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
