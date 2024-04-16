3fbd4758168785c7565ba25fc2660d196617129c
sarahallerheiligen <sballerheiligen@quinnipiac.edu>
2024-04-16 16:33:59 -0400

    searchFragment recyclerView implimented

---
+++ app/src/main/java/com/hfad/whattowatch/MovieItemAdapter.kt
@@ -0,0 +1,82 @@
+package com.hfad.whattowatch
+
+import android.content.Context
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.navigation.NavController
+import androidx.recyclerview.widget.RecyclerView
+import com.hfad.whattowatch.API.Result
+
+
+var results : ArrayList<Result> = ArrayList()
+
+
+class MovieItemAdapter(val context: Context, var navController: NavController) :
+    RecyclerView.Adapter<MovieItemAdapter.MovieItemViewHolder>() {
+
+    //itemCount used to make sure we display all the data
+    override fun getItemCount() = results.size
+
+    fun setSearchListItems(searchData: List<Result>)
+    {
+        Log.v("SearchListItems","recieved: " + searchData)
+        results = searchData as ArrayList<Result>
+        notifyDataSetChanged()
+        Log.v("SearchListItems","list: " + Result)
+
+    }
+
+    //creates view
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MovieItemViewHolder {
+
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.item_movie,parent,false)
+        return MovieItemViewHolder(view, context, navController)
+    }
+
+    //binds data to the view
+    override fun onBindViewHolder(holder: MovieItemViewHolder, position: Int) {
+        //val item = songs[position]
+        holder.bind(position)
+    }
+
+    //defines the view
+    class MovieItemViewHolder (itemView: View, private val context: Context, var navController: NavController)
+        : RecyclerView.ViewHolder(itemView) {
+
+        private val title: TextView = itemView!!.findViewById(R.id.movieName)
+        private val information: TextView = itemView!!.findViewById(R.id.movieInformation)
+
+        private var pos:Int = 0
+
+
+        //listener for detail fragment, pass along song info
+        init {
+            itemView.setOnClickListener {
+                Log.v("Navigating", "RecycleView Clicked")
+                //Search to detail fragment navigation
+                val action = SearchFragmentDirections.actionSearchFragmentToDetailFragment(pos)
+                navController.navigate(action)
+            }
+        }
+
+        fun bind(position: Int){
+
+            pos = position
+            val currMovie = results.get(position)
+            title.text = currMovie.title
+
+            //Movie Information
+            val type = currMovie.type
+            val year = currMovie.year
+            val genre = currMovie.genres[0].name
+            //val genre = genres.name
+
+            val infoText = type +"~"+ year +"~"+ genre
+
+            information.text =  infoText
+        }
+    }
+}
--- app/src/main/java/com/hfad/whattowatch/SearchFragment.kt
+++ app/src/main/java/com/hfad/whattowatch/SearchFragment.kt
@@ -6,15 +6,23 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.Navigation
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.hfad.whattowatch.API.SearchResult
 import com.hfad.whattowatch.databinding.FragmentSearchBinding
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 
+
 class SearchFragment : Fragment() {
     private var _binding: FragmentSearchBinding? = null
     private val binding get() = _binding!!
+
+    lateinit var recyclerView: RecyclerView
+    lateinit var recyclerAdapter: MovieItemAdapter
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -29,6 +37,12 @@ class SearchFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
         var search = binding.searchField
+
+        //for recyclerview
+        recyclerView = binding.searchMoviesView
+        recyclerAdapter = MovieItemAdapter(requireContext(), Navigation.findNavController(view))
+        recyclerView.layoutManager = LinearLayoutManager(requireContext())
+        recyclerView.adapter = recyclerAdapter
 
         binding.searchButton.setOnClickListener {
             Log.v("button", "Searched for " + search.text.toString())
@@ -45,7 +59,7 @@ class SearchFragment : Fragment() {
                             if (response?.body() != null) {
                                 var movies = (response.body()!! as SearchResult).result;
                                 Log.v("API Response", "movies: " + movies)
-                               // recyclerAdapter.setSearchListItems(songs)
+                                recyclerAdapter.setSearchListItems(movies)
                             }
 
                         }
@@ -60,7 +74,6 @@ class SearchFragment : Fragment() {
             }
 
         }
-
     }
 
     override fun onDestroyView() {
