1d9352e8ec89d368de6bed5b8a3edde04d2cfb81
jubck <julialbock@gmail.com>
2024-04-16 14:19:57 -0400

    navigation for favorites fragment

--- app/src/main/java/com/hfad/whattowatch/FavoritesFragment.kt
+++ app/src/main/java/com/hfad/whattowatch/FavoritesFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.findNavController
 import com.hfad.whattowatch.databinding.FragmentFavoritesBinding
 
 class FavoritesFragment : Fragment() {
@@ -14,14 +15,18 @@ class FavoritesFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-       _binding = FragmentFavoritesBinding.inflate(inflater,container, false)
+        _binding = FragmentFavoritesBinding.inflate(inflater, container, false)
         val view = binding.root
         return view
     }
-
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.returnToHome.setOnClickListener {
+            it.findNavController().navigate(R.id.action_favoritesFragment_to_homeFragment)
+        }
+    }
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
     }
-
 }
