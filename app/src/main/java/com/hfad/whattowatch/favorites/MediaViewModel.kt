23877e04b4914fb5571752c5dbd03977a1dd41fe
sarahallerheiligen <sballerheiligen@quinnipiac.edu>
2024-04-23 22:03:58 -0400

    database classes in, not working, keep troubleshooting via ch14 demo

--- app/src/main/java/com/hfad/whattowatch/favorites/MediaViewModel.kt
+++ app/src/main/java/com/hfad/whattowatch/favorites/MediaViewModel.kt
@@ -6,14 +6,14 @@ import com.hfad.whattowatch.MediaDao
 import kotlinx.coroutines.launch
 
 class MediaViewModel (val dao: MediaDao) : ViewModel() {
-    var newMediaTitle = ""
+    var newMediaTitle = "defaultTitle"
 
 
     //as of now, add Task JUST adds the title. Once that's working, we can add more info
     fun addTask() {
         viewModelScope.launch {
-            val media = Media()
-            media.mediaTitle = newMediaTitle
+            val media = Media(newMediaTitle)
+            //media.mediaTitle = newMediaTitle
             dao.insert(media)
         }
     }
