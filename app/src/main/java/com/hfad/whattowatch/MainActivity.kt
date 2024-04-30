efef8a44e1780bb026dfae9507736c8e0257ab01
charlizeaponte <charlizeaponte@yahoo.com>
2024-04-30 00:37:32 -0400

    Started implementing the toolbar

--- app/src/main/java/com/hfad/whattowatch/MainActivity.kt
+++ app/src/main/java/com/hfad/whattowatch/MainActivity.kt
@@ -1,8 +1,13 @@
 package com.hfad.whattowatch
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import android.view.View
+import android.widget.Toast
+import androidx.appcompat.widget.Toolbar
 import com.hfad.whattowatch.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -12,5 +17,39 @@ class MainActivity : AppCompatActivity() {
        binding = ActivityMainBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
+
+        val toolbar: Toolbar = findViewById(R.id.toolbar)
+        setSupportActionBar(toolbar)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.action_share -> {
+                shareContent()
+                true
+            }
+            R.id.action_about -> {
+                showAboutInfo()
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+    private fun shareContent() {
+        val shareIntent = Intent().apply {
+            action = Intent.ACTION_SEND
+            putExtra(Intent.EXTRA_TEXT, "Check out this great app!")
+            type = "text/plain"
+        }
+        startActivity(Intent.createChooser(shareIntent, null))
+    }
+    private fun showAboutInfo() {
+        Toast.makeText(this, "App version 1.0. This app uses a StreamingInformation RestAPI to look up movies/series and displays streaming information. You are able to favorite your fave movies/series and see them in your favorites list. This app was created by Charlize, SeSe, and Julia.", Toast.LENGTH_LONG).show()
     }
 }
