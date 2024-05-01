c1a0645d4bfee3661841abb7c1a716c0b438ecbc
charlizeaponte <charlizeaponte@yahoo.com>
2024-05-01 00:46:05 -0400

    fixed series year

--- app/src/main/java/com/hfad/whattowatch/DetailFragment.kt
+++ app/src/main/java/com/hfad/whattowatch/DetailFragment.kt
@@ -68,13 +68,18 @@ class DetailFragment : Fragment() {
         binding.tvMediaTitle.text = title
         val type = currMovie.type
         val year = currMovie.year
+        val showYear = currMovie.firstAirYear
         val genre = currMovie.genres[0].name
 
         val overview = currMovie.overview
         binding.tvDescription.text = overview
 
-        val infoText = type +"~"+ year +"~"+ genre
-
+        val infoText: String
+        if (type == "series") {
+            infoText = type + "~" + showYear + "~" + genre
+        } else {
+            infoText = type + "~" + year + "~" + genre
+        }
         binding.tvMediaInfo.text = infoText
 
         // Set the image based on the type of media
